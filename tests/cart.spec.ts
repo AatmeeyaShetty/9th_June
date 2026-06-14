7a7528ba6194258ad2519dbd77e636d340f81855
AatmeeyaShetty <aatmeeya.shetty@zeuslearning.com>
2026-06-14 22:45:18 +0530

    Update cart.spec.ts

--- tests/cart.spec.ts
+++ tests/cart.spec.ts
@@ -1,74 +1,37 @@
-import { test, expect } from '@playwright/test';
-import {LoginPage} from '../pages/LoginPage';
-import{ ProductPage } from '../pages/ProductsPage';
-import{CartPage} from '../pages/CartPage';
-import {users} from '../test_data/users';
-import {products} from '../test_data/products';
-
-
-
-async function login(page: any){
-    const standardUser = users.find(user =>user.type ==='standard');
-    await page.goto('https://www.saucedemo.com/');
-    await page.getByPlaceholder('Username').fill(standardUser!.username);
-    await page.getByPlaceholder('Password').fill(standardUser!.password);
-    await page.getByRole('Button',{name: 'Login'}).click();
-}
-test.describe("Cart tests ",() =>{
-    test('TC_006 - Add Product to cart @cart @smoke',async ({page}) =>{
-        const loginPage = new LoginPage(page);
+import { test } from '@playwright/test';
+import { ProductPage } from '../pages/ProductsPage';
+import { CartPage } from '../pages/CartPage';
+import { products } from '../test-data/products';
+import { loginAs } from '../utils/testHelpers';
+const [firstProduct, secondProduct] = products;
+test.describe('Cart Tests', () => {
+   test('TC_006 - Add Product to cart @cart @smoke', async ({ page }) => {
         const productsPage = new ProductPage(page);
-        const cartPage = new CartPage(page);
-        const standardUser=users.find(user => user.type === 'standard');
-        if(!standardUser){
-            throw new Error('Standard user not found');
-        }
-        await loginPage.goto();
-        await loginPage.login(standardUser.username,standardUser.password);
-        await productsPage.addProductToCart(products[0].name);
+        await loginAs(page);
+        await productsPage.addProductToCart(firstProduct.name);
         await productsPage.verifyCartCount(1);
     });
-
-
-    test('TC_007 - Remove  Product to cart @cart @regression',async ({page}) =>{
-        const loginPage = new LoginPage(page);
+    test('TC_007 - Remove Product from cart @cart @regression', async ({ page }) => {
         const productsPage = new ProductPage(page);
-        const standardUser=users.find(user => user.type === 'standard');
-        if(!standardUser){
-            throw new Error('Standard user not found');
-        }
-        await loginPage.goto();
-        await loginPage.login(standardUser.username,standardUser.password);
-        await productsPage.addProductToCart(products[0].name);
-        await productsPage.removeProductFromCart(products[0].name);
+        await loginAs(page);
+        await productsPage.addProductToCart(firstProduct.name);
+        await productsPage.removeProductFromCart(firstProduct.name);
         await productsPage.verifyCartCount(0);
     });
-    test('TC_008 - Add Multiple Products @cart @regression',async ({page}) =>{
-        const loginPage = new LoginPage(page);
+    test('TC_008 - Add Multiple Products @cart @regression', async ({ page }) => {
         const productsPage = new ProductPage(page);
-        const standardUser=users.find(user => user.type === 'standard');
-        if(!standardUser){
-            throw new Error('Standard user not found');
-        }
-        await loginPage.goto();
-        await loginPage.login(standardUser.username,standardUser.password);
-        await productsPage.addProductToCart(products[0].name);
-        await productsPage.addProductToCart(products[1].name);
+        await loginAs(page);
+        await productsPage.addProductToCart(firstProduct.name);
+        await productsPage.addProductToCart(secondProduct.name);
         await productsPage.verifyCartCount(2);
     });
-    test('TC_009 - Verify product in the cart @cart @regression',async ({page}) =>{
-        const loginPage = new LoginPage(page);
+    test('TC_009 - Verify product in cart @cart @regression', async ({ page }) => {
         const productsPage = new ProductPage(page);
-        const standardUser=users.find(user => user.type === 'standard');
-        if(!standardUser){
-            throw new Error('Standard user not found');
-        }
-        await loginPage.goto();
-        await loginPage.login(standardUser.username,standardUser.password);
-        await productsPage.addProductToCart(products[0].name);
-        await productsPage.goToCart();
         const cartPage = new CartPage(page);
-        await cartPage.verifyProductInCart(products[0].name);
+        await loginAs(page);
+        await productsPage.addProductToCart(firstProduct.name);
+        await productsPage.goToCart();
+        await cartPage.verifyProductInCart(firstProduct.name);
     });
 
-    });
+});
